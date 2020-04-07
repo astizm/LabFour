84f2d80d40073e9a5912b1f553d54e8545f95481
astizm <asti-maera@live.nmit.ac.nz>
2020-04-07 21:07:24 +1200

    simplified and added more notes

--- index.js
+++ index.js
@@ -11,13 +11,20 @@ console.log(decimalNumber(1101))
 
 first attempt*/
 
-// Second attempt
+// Second(22th) attempt
 function decimalNumber(x){
-    var x = 1010101111
-    console.log(parseInt(x, 2)) //687
-   console.log(Math.pow(2, 10)) //1024
+    
+    return (parseInt(x, 2))
+
 
 }
-console.log(decimalNumber(1010))
+console.log(decimalNumber(111111110))
 
-//Probably should have done some sort of array and an if/while loop. my braind hurts and it's dinner time...
+/*Probably should have done some sort of if/while loop....
+Really, I'm only about half way there through most of my coding at this point.
+Just have to keep researching and figuring out what the solution might be.
+Also to stop half-arsing ten different methods and follow through on one method instead of the above.
+Tricky when there's so many different ways to do one thing and trying to decide on the solution that best fits.
+A lot of information overload and conflicting opinions on methods.
+Going to carry on learning basics and */
+

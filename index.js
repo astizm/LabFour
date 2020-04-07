a40cfd6c3ecad54ce78b80b05d787021934f5e2a
astizm <asti-maera@live.nmit.ac.nz>
2020-04-07 21:09:42 +1200

    final

--- index.js
+++ index.js
@@ -18,13 +18,8 @@ function decimalNumber(x){
 
 
 }
-console.log(decimalNumber(111111110))
+console.log(decimalNumber(111111110)) //510
 
-/*Probably should have done some sort of if/while loop....
-Really, I'm only about half way there through most of my coding at this point.
-Just have to keep researching and figuring out what the solution might be.
-Also to stop half-arsing ten different methods and follow through on one method instead of the above.
-Tricky when there's so many different ways to do one thing and trying to decide on the solution that best fits.
-A lot of information overload and conflicting opinions on methods.
-Going to carry on learning basics and */
+/*Seems about right?
+*/
 
